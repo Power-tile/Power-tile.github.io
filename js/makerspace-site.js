3e1cd203540f13df6bf2e82871ce80f4700cc53f
Power_tile <hejizheng2004@163.com>
2020-07-01 14:23:25 +0800

    add ip handbook, add scp makerspace finished, add various pics + scp club competition imgs

---
+++ js/makerspace-site.js
@@ -0,0 +1,47 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        makers: [
+            {
+                groupName: "2020 Spring Maker Course",
+                people: ["Jaguar Kristeller", "Jerry Zhang", "Peter Lian"]
+            },
+            {
+                groupName: "Climate Mitigation Solar IP",
+                people: ["Clef Song", "Ziyang Liu", "Aimee Deng", "Laura Liu", "Kolento Hou", "Lidiia", "Cindy Wang", "Jonathan", "Laurence Lin", "Jaguar Kristeller", "Daniel He", "Billy Wang", "Frank Yin", "Samson Huang", "John Yang", "Jerry Zhang", "Michael Su", "Rainy"]
+            },
+            {
+                groupName: "HTMA  Makers",
+                people: ["Deltas Shi", "Ugo Cheung", "Steven Huang"]
+            },
+            {
+                groupName: "SCP Makers",
+                people: ["Roy Gao", "Grace Xie", "Vans Wang", "Dana Gretton", "Simon (Aubrey)", "Howen", "Bella", "Teresa", "Peter Kristeller", "Mitch Altman", "Kabong"]
+            },
+            {
+                groupName: "Smart MSA Makers",
+                people: ["Blue", "Sabrina Cao", "Kira Ding", "Emma Liu", "Jacob Yuan", "Susan Xu"]
+            },
+            {
+                groupName: "Acdemic Challenge Makers",
+                people: ["Serena Li", "Jessica Wang", "Stacy Zhao", "Cherry Zhao", "Cherry Zheng", "Astrid Zhang", "Laura Liu", "Seana He", "John Yang", "Brown Wang", "Karissa Liu", "Joseph Sun", "Aubrey Chen"]
+            },
+            {
+                groupName: "Infrequent Makers",
+                people: ["Riley Zeng", "Steve Ye", "Serene Zhang", "Zero Dong", "Sandy Han", "Aimee Du", "Johnson Chi", "Angela Wu", "Daniel Yu", "Henry Xia", "Wing Li", "Sunny Wang", "Laila Jia", "Tommy Chen", "RObinson Yang", "Olivia Huang", "Sally Fang", "Lily Li", "Andy Song", "Coco Qin", "Kiwi He", "Eric Zheng", "Sam Yu", "Rina Zhu", "Luna Liu", "Nova Yuan", "Ziyang Liu"]
+            },
+            {
+                groupName: "Guardian Makers",
+                people: ["Blue", "Jerry Jia", "Zhou Zhou", "Meddy", "Yuan de Man", "Sarah", "Petula", "Joel", "George", "Zale", "Yif", "Andrew", "Tommy"]
+            },
+            {
+                groupName: "All Leaders",
+                people: ["Jaguar", "Daniel He", "Samson", "Billy", "Roy", "Laurence", "Kevin BA", "Cherry", "Kevin Sun", "Elina"]
+            },
+            {
+                groupName: "Current Leadership Members",
+                people: ["Jaguar", "Daniel He", "Samson", "Billy", "Roy"]
+            }
+        ]
+    }
+})
